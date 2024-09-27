40ee6a86a70c50bd41e1b9d4dfc41533412bd026
nlnlsofun <nlnlsofun@cht.com.tw>
2024-09-27 18:07:43 +0800

    delete_c

---
+++ column_modify.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import sqlite3
+   
+def add_column(db_address, add_c, add_text):
+# 連接到 SQLite 資料庫
+    with sqlite3.connect(db_address) as conn:
+        c = conn.cursor()  # 建立 cursor
+        
+        sql_alter = f'''
+        ALTER TABLE customers ADD COLUMN "{add_c}" TEXT;
+        '''
+        c.execute(sql_alter)  # 執行 ALTER TABLE 語句
+    
+        sql_update = f'''
+        UPDATE customers SET "{add_c}" = "{add_text}";
+        '''
+        c.execute(sql_update)  # 更新所有資料的 Status 欄位
+        
+        # 確認改動
+        conn.commit()
+        
+        i_c_check = "add_OK"
+        return i_c_check
+
+
+def delete_column(db_address,delete_c):
+# 連接到 SQLite 資料庫
+    with sqlite3.connect(db_address) as conn:
+        c = conn.cursor()  # 建立 cursor
+        
+        sql_delete = f'''
+        ALTER TABLE customers DROP COLUMN "{delete_c}" ;
+        '''
+        c.execute(sql_delete)  # 執行 ALTER TABLE 語句
+
+        
+        # 確認改動
+        conn.commit()
+        
+        d_c_check = "delete_OK"
+        return d_c_check
--- main_merge.py
+++ main_merge.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import sqlite3
-import insert_csv
-from insert_csv import insert_csv
-from add_column import add_column
+import insert_csv as csv
+import column_modify as cm
 
-text = add_column()
-print(text)
+csv_address = ".\csv\customers-100.csv"
+table_name = "customers"
+db_address = ".\db\customers-100.db" 
+add_c = "期數"
+add_text = "108"
+delete_c = "期數"
 
+select_check = csv.insert_csv (csv_address,db_address,table_name)
+print(select_check)
+
+
+#c_return = cm.add_column(db_address,add_c,add_text)
+#text = cm.delete_column(db_address,delete_c)
+#print(c_return)
