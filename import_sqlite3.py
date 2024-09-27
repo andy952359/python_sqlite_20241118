398e3b2d091b3f00ecb94a27abb0f2f7827c1811
nlnlsofun <nlnlsofun@cht.com.tw>
2024-09-27 11:49:02 +0800

    git_insert_customer-100

--- import_sqlite3.py
+++ import_sqlite3.py
@@ -1,4 +1,9 @@
+import pandas as pd
 import sqlite3
 
+df = pd.read_csv('customers-100.csv')  #讀取CSV資料集檔案
+print(df)
 
-print ("hello")
+with sqlite3.connect('customers-100.db') as conn100:
+    df.to_sql('customers', conn100, if_exists='replace', index=False) #新增資料表
+
