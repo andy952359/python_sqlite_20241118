c9dee653e669f5f11eb2ae803acd2ae4419ff9f2
nlnlsofun <nlnlsofun@cht.com.tw>
2024-10-22 17:07:37 +0800

    insert2_1022

---
+++ insert2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sqlite3
+
+def insert_csv (csv_address,db_address,table_name):
+    df = pd.read_csv(csv_address)  #讀取CSV資料集檔案
+
+    with sqlite3.connect(db_address) as conn:
+        df.to_sql(table_name, conn, if_exists='replace', index=False) #新增資料表
+        select_check = pd.read_sql(f"SELECT * FROM  {table_name} WHERE Country='Togo'", conn)
+        return select_check
--- main_merge.py
+++ main_merge.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import sqlite3
 import insert_csv as csv
+import insert2 
 import column_modify as cm
 
 csv_address = ".\csv\customers-100.csv"
+csv_address2 = ".\csv\customers-101.csv"
 table_name = "customers"
 db_address = ".\db\customers-100.db" 
 add_c = "期數"
